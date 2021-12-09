ab7c854b129985ded9da39820f50e7d7f63c1077
Kelvin <kelvinjune2@chaka.ng>
2021-12-09 22:57:13 +0100

    added prop types

---
+++ src/components/Movie.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import classes from './Movie.module.css';
+
+const Movie = (props) => {
+  return (
+    <li className={classes.movie}>
+      <h2>{props.title}</h2>
+      <h3>{props.releaseDate}</h3>
+      <p>{props.openingText}</p>
+    </li>
+  );
+};
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  openingText: PropTypes.string.isRequired,
+};
+export default Movie;
---
+++ src/components/MoviesList.js
@@ -0,0 +1,27 @@
+// import PropTypes from 'prop-types';
+
+import React from 'react';
+
+import Movie from './Movie';
+import classes from './MoviesList.module.css';
+
+const MovieList = (props) => {
+  return (
+    <ul className={classes['movies-list']}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  );
+};
+
+MovieList.propTypes = {
+  movies: React.element.required,
+};
+
+export default MovieList;
